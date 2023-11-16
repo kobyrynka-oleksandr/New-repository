a1d3ffc07a1be26e319e21bf2bfc67e366d17259
kobyrynka-oleksandr <kobyrynka.oleksandr1123@vu.cdu.edu.ua>
2023-11-16 23:38:47 +0200

    Виведення Hello World!

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace New_project
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
